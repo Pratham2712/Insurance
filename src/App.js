2f591b075aeab00ab7c59372659175e37f76efa4
Pratham2712 <prathamvaishya123@gmail.com>
2021-09-30 13:43:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import Nav from './components/Nav';
+//Styles
 import GlobalStyles from './GlobalStyles';
+//import components
+import Nav from './components/Nav';
 import UpperNav from './components/UpperNav';
 import Home from './pages/Home';
+import Contact from './pages/ContactPage';
+import Footer from './components/Footer';
+//page animation
+import { AnimatePresence } from 'framer-motion';
+//Router
+import { Switch, Route , useLocation } from 'react-router-dom';
 
 function App() {
+	const location = useLocation();
 	return (
 		<div className="App">
 			<GlobalStyles />
 			<UpperNav />
-			<Nav />
-			<Home />
+			<Nav
+			/>
+			<AnimatePresence initial={false} exitBeforeEnter>
+			<Switch location={location} key={location.pathname}>
+				<Route path="/" exact>
+			        <Home />
+			    </Route>
+				<Route path="/contact">
+			        <Contact />
+			    </Route>
+			</Switch>
+			</AnimatePresence>
+			<Footer/>
 		</div>
 	);
 }
